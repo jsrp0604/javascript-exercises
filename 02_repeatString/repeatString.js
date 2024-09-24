36ea7460df8e543a1b3c6312b863bce275abe470
jsramirez <jsramirez@uees.edu.com>
2024-09-24 18:08:31 -0500

    Completed 2nd Exercise
    Simple for loop with if conditional for error verification

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, nTimes) {
+    let finalString = '';
+    
+    if (nTimes < 0 ) return 'ERROR';
 
+    for (let i = 0; i < nTimes; i++) {
+        finalString = finalString + string;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
