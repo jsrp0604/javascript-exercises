051e7233b9f82ce933f019060a2377a15109f2f3
jsramirez <jsramirez@uees.edu.com>
2024-09-26 14:05:56 -0500

    Finished 6th exercise leapYears
    Implemented with nested if conditionals

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) { 
 
+    // Must be divisible by 4, divisible by 100 if and only if divisible by 400
+    if (year % 4 == 0)  {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) 
+                return true;
+            return false;  
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
