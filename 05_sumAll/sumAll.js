0c1d567102a8785510cb656a81775a5d5c0f484e
jsramirez <jsramirez@uees.edu.com>
2024-09-26 01:39:16 -0500

    Finished 5th exercise sumAll
    Simple for loop, implemented all required conditionals for errors

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let finalSum = 0;
 
+    let isNegative = firstInt < 0 || secondInt < 0;
+    let isInt = Number.isInteger(firstInt) && Number.isInteger(secondInt);
+    let isNumber = (typeof firstInt == "number") && (typeof secondInt == "number");  
+
+    if (isNegative || !isInt || !isNumber) return 'ERROR';
+
+    if (firstInt > secondInt) {
+        let temp = firstInt;
+        firstInt = secondInt;
+        secondInt = temp;
+    }
+
+    for (let i = firstInt; i <= secondInt; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
+
+console.log(sumAll(2.5, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
