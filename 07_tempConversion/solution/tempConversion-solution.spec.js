160767a08ff7ba24486a52efd7638d249f2423b8
jsramirez <jsramirez@uees.edu.com>
2024-09-26 14:22:56 -0500

    Finished 7th exercise tempConversion
    Implemented toFixed() and parseFloat() methods for solution

--- 07_tempConversion/solution/tempConversion-solution.spec.js
+++ 07_tempConversion/solution/tempConversion-solution.spec.js
@@ -4,19 +4,19 @@ const {
 } = require('./tempConversion-solution');
 
 describe('convertToCelsius', () => {
-  test('works', () => {
+  test.skip('works', () => {
     expect(convertToCelsius(32)).toEqual(0);
   });
-  test('rounds to 1 decimal', () => {
+  test.skip('rounds to 1 decimal', () => {
     expect(convertToCelsius(100)).toEqual(37.8);
   });
-  test('works with negatives', () => {
+  test.skip('works with negatives', () => {
     expect(convertToCelsius(-100)).toEqual(-73.3);
   });
 });
 
 describe('convertToFahrenheit', () => {
-  test('works', () => {
+  tes('works', () => {
     expect(convertToFahrenheit(0)).toEqual(32);
   });
   test('rounds to 1 decimal', () => {
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celciusTemp = (temp - 32) * (5/9);
+  let fixedDecimalCelciusTemp = parseFloat(celciusTemp.toFixed(1)) 
+
+  return fixedDecimalCelciusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitTemp = temp * 9/5 + 32;
+  let fixedDecimalFahrenheitTemp = parseFloat(fahrenheitTemp.toFixed(1)) 
+
+  return fixedDecimalFahrenheitTemp;
 };
 
 // Do not edit below this line
