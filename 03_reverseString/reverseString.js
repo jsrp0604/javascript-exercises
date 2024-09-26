a2474ff763c0f06f157808b24768f7637d4e20b2
jsramirez <jsramirez@uees.edu.com>
2024-09-25 22:55:44 -0500

    Finished exercise 3
    Implemented function to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) { 
+    let reversedString = '';
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString = reversedString + string[i];
+    }
+
+    return reversedString;
 
 };
 
