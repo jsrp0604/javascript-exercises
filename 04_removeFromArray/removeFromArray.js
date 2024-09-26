53f597b187ae907850d1d0b3e72a8f8e0a7076a6
jsramirez <jsramirez@uees.edu.com>
2024-09-26 01:06:53 -0500

    Finished 4th exercise removeFromArray
    Implemented arguments object and filter function with include()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const argumentsArray = Array.from(args); 
+
+    let newArray = array.filter((element) => !argumentsArray.includes(element));
+    
+    return newArray;
 };
 
 // Do not edit below this line
