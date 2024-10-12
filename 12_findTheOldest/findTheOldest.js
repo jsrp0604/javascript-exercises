5fbadf6c8478b6fe863860649df5754a3d18c9d5
jsramirez <jsramirez@uees.edu.com>
2024-10-11 20:49:08 -0500

    Finished 12th exercise
    Implemented with forEach() loop and if conditionals

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let greatestAge = 0;
+    let oldestPerson;
 
+    array.forEach(person => {
+        if (person.yearOfDeath === undefined)  {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+
+        let age = person.yearOfDeath - person.yearOfBirth; 
+
+        if (age > greatestAge) {
+            greatestAge = age;
+            oldestPerson = person;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
