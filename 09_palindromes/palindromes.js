5ab6742b5b9de3de3aba22ebfcc79370da2f14b5
jsramirez <jsramirez@uees.edu.com>
2024-10-11 19:33:26 -0500

    Finished 9th exercise
    Compared filteredStr with reverseStr using array and string methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let regex = /[^\w\s]+/;
 
+    let filteredStr = str.split("").filter(char => !char.match(regex) && char != " ").join("").toLowerCase();
+    let reversedStr = filteredStr.split("").reverse().join("");
+
+    return filteredStr === reversedStr;
 };
 
 // Do not edit below this line
